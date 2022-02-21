ee9dca69c7bd0d76029cfc79883542c1426087d9
phelixdusengimana <phelixdusengimana@gmail.com>
2022-02-21 20:55:31 +0200

    rendering html file, redirecting, urls and views

--- 03 View, Routing, URLs/my_site/my_app/views.py
+++ 03 View, Routing, URLs/my_site/my_app/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
+
+# articles = {
+#     "sports": "Sport page",
+#     "finance": "finance page",
+#     "politics": "politics page",
+# }
+#
+#
+# def news_view(request, topic):
+#     try:
+#         return HttpResponse(articles[topic])
+#     except:
+#         raise Http404("Page not found.")
+#
+#
+# def add_view(request, num1, num2):
+#     result = f"{num1} + {num2} =  {num1 + num2}"
+#     return HttpResponse(str(result))
+#
+#
+# def num_page_view(request, num_page):  # Redirecting
+#     topics_list = list(articles.keys())
+#     try:
+#         topic_name = topics_list[num_page]
+#         webpage = reverse("topic_page", args=[topic_name])  # Reverse URL
+#         return HttpResponseRedirect(webpage)
+#     except:
+#         raise Http404("Not founddd")
+
+def simple_view(request):
+    return render(request, 'my_app/example.html')  # .html
---
+++ 03 View, Routing, URLs/notes.md
@@ -0,0 +1,14 @@
+ View function takes argument of `request` else it provides error.
+
+ # Dynamic routing
+
+We use <data_type:name_of_variable> e.g: "<str:name>/"
+
+# Using ResponseNotFound
+
+we can import `HttpResponseNotFound` or `Http404` from django.http
+
+# Redirecting
+we use `HttpResponseRedirects`
+
+# Reverse URL and URL names
