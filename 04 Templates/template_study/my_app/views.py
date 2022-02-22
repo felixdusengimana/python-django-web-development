f92d1e2a7cabe8e79cddee6152a2080f6daddc78
phelixdusengimana <phelixdusengimana@gmail.com>
2022-02-22 10:16:19 +0200

    passing informations to a html file from views

--- 04 Templates/template_study/my_app/views.py
+++ 04 Templates/template_study/my_app/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 def example_view(request):
     return render(request, 'my_app/example.html')
+
+
+def variable_view(request):
+    dicty = {
+        "fist_name": "Felix",
+        "last_name": "Wazekwa",
+        "some_list": [1, 2, 3],
+        "some_dict": {"inside_key": "inside_value"}
+    }
+    return render(request=request, template_name="my_app/variable.html", context=dicty)
