4fe22c6c52dcfc86229cb072d1426f6fb0d779c0
phelixdusengimana <phelixdusengimana@gmail.com>
2022-02-22 11:01:06 +0200

    rendering static files

--- 04 Templates/template_study/template_study/settings.py
+++ 04 Templates/template_study/template_study/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jobi6+-%drb16j--a!cpaf!_sc*7de+$@m=rxt+l@9t@b@+hxo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
