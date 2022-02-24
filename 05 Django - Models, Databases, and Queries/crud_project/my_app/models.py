9eb7abbb7e2d9d6fdbd057e8feb781912fd238b1
phelixdusengimana <phelixdusengimana@gmail.com>
2022-02-24 19:14:52 +0200

    manually change how data are returned

--- 05 Django - Models, Databases, and Queries/crud_project/my_app/models.py
+++ 05 Django - Models, Databases, and Queries/crud_project/my_app/models.py
@@ -7,3 +7,6 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     age = models.IntegerField()
+
+    # def __str__(self):
+    #     return f"{self.last_name},{self.first_name},{self.age}"
